5abba2f10f1be521815325f06c6255be520d6ed0
Vlad <vlad@example.com>
2021-11-27 18:51:41 +0300

    added reset feature and moved elevator code

--- src/game.ts
+++ src/game.ts
@@ -6,6 +6,7 @@ import {
  } from "helpers"
 import { whitePieces, blackPieces } from "pieces"
 import { addBillboard } from "billboard"
+import { spawnElevators } from "elevators"
 import { sceneMessageBus } from "messageBus"
 
 const WHITE = "white"
@@ -290,88 +291,34 @@ export class FloatMove implements ISystem {
 
 engine.addSystem(new FloatMove())
 
-// Elevator logic
+spawnElevators()
 
-@Component("lerpData")
-export class LerpData {
-  origin: Vector3 = Vector3.Zero()
-  target: Vector3 = Vector3.Zero()
-  fraction: number = 0
-  constructor(origin: Vector3, target: Vector3, fraction: number) {
-    this.origin = origin
-    this.target = target
-    this.fraction = fraction
-  }
-}
+// Reset game
+let button = spawnEntity(new BoxShape(), new Vector3(3.5,0,2.5))
 
-export class LerpMove implements ISystem {
-  update(dt: number) {
-    for (let el of elevatorGroup.entities){
-      if (el.hasComponent(LerpData)) {
-        let transform = el.getComponent(Transform)
-        let lerp = el.getComponent(LerpData)
-        if (lerp.fraction < 1) {
-          transform.position = Vector3.Lerp(lerp.origin, lerp.target, lerp.fraction)
-          lerp.fraction += dt / 6
-        } else {
-          engine.removeSystem(new LerpMove())
-        }
+button.addComponent(new OnPointerDown((e)=>{
+  sceneMessageBus.emit("reset", {})
+}))
 
-      }
-
+function reset(){
+    while (boxGroup.entities.length) {
+      engine.removeEntity(boxGroup.entities[0])
     }
-  }
+    while (pieceGroup.entities.length) {
+      engine.removeEntity(pieceGroup.entities[0])
+    }
+
+  initBoard()  
 }
 
-const ethOrigin = new Vector3(8, 0, 2)
-const bnbOrigin = new Vector3(8, 0, 14)
+sceneMessageBus.on("reset", ()=>{reset()})
 
-@Component("isElevator")
-export class IsElevator {}
-
-function initElevator(origin: Vector3, side: string) {
-  const elevator = spawnEntity(new BoxShape(), origin)
-  elevator.getComponent(Transform).scale = new Vector3(2, 0.1, 3)
-  elevator.addComponent(new IsElevator())
-  return elevator
+function initBoard(): void {
+    turn = WHITE;
+    currentInHand = null
+    makeChessBoard();
+    addPieces();
+    enableInteractablePiece(true);
 }
 
-const ethElevator = initElevator(ethOrigin, "eth")
-const bnbElevator = initElevator(bnbOrigin, "bnb")
-
-const elevatorGroup = engine.getComponentGroup(IsElevator)
-
-for (let el of elevatorGroup.entities) {
-  const pos = el.getComponent(Transform).position
-  const posTop = new Vector3(pos.x , 4, pos.z)
-  const posBottom = new Vector3(pos.x , 0, pos.z)
-  const onClickUp = new OnPointerDown(() => {
-    el.addComponentOrReplace(new LerpData(el.getComponent(Transform).position, posTop, 0))
-    if (el.hasComponent(OnPointerDown))
-      el.removeComponent(OnPointerDown)
-    el.addComponent(onClickDown)
-    engine.addSystem(new LerpMove())
-  },
-  {
-    hoverText: "Press to go UP",
-    distance: 6
-  })
-  const onClickDown = new OnPointerDown(() => {
-    el.addComponentOrReplace(new LerpData(el.getComponent(Transform).position, posBottom, 0))
-    if (el.hasComponent(OnPointerDown))
-      el.removeComponent(OnPointerDown)
-    el.addComponent(onClickUp)
-    engine.addSystem(new LerpMove())
-  },
-  {
-    hoverText: "Press to go DOWN",
-    distance: 6
-  })
-  
-  el.addComponent(onClickUp)
-}
-
-
-makeChessBoard()
-addPieces()
-enableInteractablePiece(true)
+initBoard()
