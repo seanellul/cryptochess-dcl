f938895a19ba30fe9f80b18506ef201ada5f6128
Vlad <vlad@example.com>
2021-11-27 19:14:49 +0300

    updated some figures

--- src/pieces.ts
+++ src/pieces.ts
@@ -1,18 +1,18 @@
 class WhitePiece {
-    public King = {number: 1, name: "King", model: new GLTFShape("models/King.glb")}
+    public King = {number: 1, name: "King", model: new GLTFShape("models/Etherium_King.glb")}
     public Pawn = {number: 2, name: "Pawn", model: new GLTFShape("models/Etherum.glb")}
     public Knight = {number: 3, name: "Knight", model: new GLTFShape("models/Horse_White.glb")}
-    public Bishop = {number: 4, name: "Bishop", model: new GLTFShape("models/Slon_white.glb")}
-    public Rook = {number: 5, name: "Rook", model: new GLTFShape("models/Ladja_white.glb")}
-    public Queen = {number: 6, name: "Queen", model: new GLTFShape("models/Queen_white.glb")}
+    public Bishop = {number: 4, name: "Bishop", model: new GLTFShape("models/Etherium_Slon.glb")}
+    public Rook = {number: 5, name: "Rook", model: new GLTFShape("models/Etherium_Rook.glb")}
+    public Queen = {number: 6, name: "Queen", model: new GLTFShape("models/Etherium_Queen.glb")}
 }
 class BlackPiece {
-    public King = {number: 1, name: "King", model: new GLTFShape("models/King.glb")}
+    public King = {number: 1, name: "King", model: new GLTFShape("models/Binance_King.glb")}
     public Pawn = {number: 2, name: "Pawn", model: new GLTFShape("models/Binance.glb")}
     public Knight = {number: 3, name: "Knight", model: new GLTFShape("models/Horse_White.glb")}
-    public Bishop = {number: 4, name: "Bishop", model: new GLTFShape("models/Slon_Black.glb")}
-    public Rook = {number: 5, name: "Rook", model: new GLTFShape("models/Ladja_black.glb")}
-    public Queen = {number: 6, name: "Queen", model: new GLTFShape("models/Queen_black.glb")}
+    public Bishop = {number: 4, name: "Bishop", model: new GLTFShape("models/Binance_Slon.glb")}
+    public Rook = {number: 5, name: "Rook", model: new GLTFShape("models/Binance_Rook.glb")}
+    public Queen = {number: 6, name: "Queen", model: new GLTFShape("models/Binance_Queen.glb")}
 }
 
 const whitePiece = new WhitePiece()
